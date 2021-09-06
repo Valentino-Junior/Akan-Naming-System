d156627a512ffbc4ee5bf647d4386442816c746e
valentine-ochieng <valentine.ochieng@student.moringaschool.com>
2021-09-06 08:42:00 +0300

    Added styling to the main.js file

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-var akan = function(y, m, d, g) {
-    var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+var akanName = function(y, m, d, g) {
     var maleNames = ["Kwasi", "Kudwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+   
     var d = new Date(y, --m, d);
     if (g === "Female") {
         return d && femaleNames[d.getDay()];
@@ -8,3 +9,18 @@ var akan = function(y, m, d, g) {
         return d && maleNames[d.getDay()];
     }
 }
+
+$(document).ready(function() {
+    $(".button").click(function(event) {
+        event.preventDefault();
+        var y = parseInt($(".year").val());
+        var m = parseInt($(".month").val());
+        var d = parseInt($(".date").val());
+        var g = $("select").val();
+        var result = akanName(y, m, d, g);
+        $("span").html("Your akan name is: " + result)
+        //alert("Your akan name is: " + result);
+        //refresh page
+        
+    });
+});
